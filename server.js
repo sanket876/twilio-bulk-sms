48caf053db142160ca9f736fd4c17a00c0d50662
Sanket Gandhi <sanketgandhi876@gmail.com>
2017-07-15 20:30:02 +0530

    Initial installation
    We have express, morgan and ejs

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+var morgan = require('morgan');
+
+// morgan will be the middle ware b/w client and app server. Each request will be going through morgan
+app.use(morgan('dev'));
+
+app.use('/', function(request, response) {
+	response.send('Or first express progrma');
+});
+
+app.listen(port);
+
+console.log('Server running on port : ' + port);
