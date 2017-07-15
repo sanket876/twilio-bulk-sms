1c42d8b5b96f83eb0370654c483972ceaaafe4f8
Sanket Gandhi <sanketgandhi876@gmail.com>
2017-07-15 20:47:09 +0530

    Session Management
    Cookie-parser and Express-Session

--- server.js
+++ server.js
@@ -2,13 +2,25 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
+var cookieParser = require('cookie-parser');
+var session  =require('express-session');
 var morgan = require('morgan');
 
 // morgan will be the middle ware b/w client and app server. Each request will be going through morgan
 app.use(morgan('dev'));
 
+app.use(cookieParser());
+app.use(session({ secret: 'sanketString',
+				  saveUninitialized: true,
+				  resave: true}));
+
 app.use('/', function(request, response) {
-	response.send('Or first express progrma');
+	console.log("New Request has been arrived!!!");
+	response.send('Our first express program');
+	console.log(request.cookies);
+	console.log("=======================");
+	console.log(request.session);
+	console.log("=======================");
 });
 
 app.listen(port);
